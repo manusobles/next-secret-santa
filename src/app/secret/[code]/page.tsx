e477e95e83422021bc2acc4749c7739a9f9b97de
Manu Sobles <manu@manusobles.com>
2023-12-02 19:15:16 +0100

    feat: style secret page

--- src/app/secret/[code]/page.tsx
+++ src/app/secret/[code]/page.tsx
@@ -1,4 +1,6 @@
 import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 export const metadata: Metadata = {
@@ -20,10 +22,37 @@ export default async function Page({ params }: { params: { code: string } }) {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>
-        Tu regalo será para: <b>{decodedSecret}</b>
-      </p>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+      <Image
+        src="/header.jpg"
+        alt="Header"
+        className="absolute inset-x-0 top-0 z-0 w-full"
+        width="500"
+        height="200"
+      />
+      <div className="sm:flex sm:items-start z-10 mt-10">
+        <div className="text-center sm:mt-10 sm:text-left">
+          <h2
+            className="text-base text-center font-semibold leading-6 text-gray-900"
+            id="modal-title"
+          >
+            Tu amigo/a invisible es:
+          </h2>
+          <div className="text-center">
+            ¡<b>{decodedSecret}</b>!
+          </div>
+          <div className="mt-4">
+            <p className="text-sm text-gray-500">
+              Acuérdate de comprarle un detalle 😊
+            </p>
+          </div>
+        </div>
+      </div>
+      <Link href="https://github.com/manusobles">
+        <div className="inline-flex items-center z-10 rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
+          Made with ❤️ by Manu
+        </div>
+      </Link>
     </main>
   );
 }
